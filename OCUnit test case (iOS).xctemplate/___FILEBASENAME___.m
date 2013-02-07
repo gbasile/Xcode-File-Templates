b1eb73541dd2eae5c299ac3ec4bba2eeadbce53f
Giuseppe Basile <rosanero@gmail.com>
2013-02-07 16:24:51 +0100

    My Xcode Template for Unit Testing with OCHamcrest and OCMock

---
+++ OCUnit test case (iOS).xctemplate/___FILEBASENAME___.m
@@ -0,0 +1,29 @@
+//
+//  ___PROJECTNAME___ - ___FILENAME___
+//  Copyright ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+//  Created by: ___FULLUSERNAME___
+//
+
+#import "<#Class Header#>"
+
+#import <SenTestingKit/SenTestingKit.h>
+
+#define HC_SHORTHAND
+#import <OCHamcrest/OCHamcrest.h>
+#import <OCMock/OCMock.h>
+
+@interface ___FILEBASENAMEASIDENTIFIER___ : SenTestCase
+@end
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+{
+    // test fixture ivars go here
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ___FILEBASENAMEASIDENTIFIER___");
+}
+
+@end
